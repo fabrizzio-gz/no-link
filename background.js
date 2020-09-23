ab9780333a87c98e22a8222c110e547e434ec4e2
fabrizzio-gz <fabrizzio.gz@gmail.com>
2020-09-23 19:23:59 -0400

    Formatting content.js

--- background.js
+++ background.js
@@ -1,65 +1,55 @@
+const showLinkIcon = {
+};
+
 function onError(error) {
-  browser.browserAction.setIcon({path: "icons/link-icon.png"});
-  console.error(`Error: ${error}`);
+    browser.browserAction.setIcon({path: "icons/link-icon.png"});
 }
 
 // Send message to active tabs to activate content JS
 function sendMessageToTabs(tabs) {
-  for (let tab of tabs) {
+    for (let tab of tabs) {
+        browser.tabs.sendMessage(
+            tab.id,
+            {isQuery: false}
+        ).then(response => {
+        
+            if (response.hideLinks) {
+                browser.browserAction.setIcon({path: "icons/broken-link-icon.png"});
+            } else {
+                browser.browserAction.setIcon({path: "icons/link-icon.png"});
+            }
+
+        }).catch(onError);
+    }
+}
+
+
+function verifyTabStatus(activeInfo) {
+    
     browser.tabs.sendMessage(
-      tab.id,
-        {isQuery: false}
+        activeInfo.tabId,
+        {
+            isQuery: true
+        }
     ).then(response => {
         
         if (response.hideLinks) {
-             browser.browserAction.setIcon({path: "icons/broken-link-icon.png"});
+            browser.browserAction.setIcon({path: "icons/broken-link-icon.png"});
         } else {
             browser.browserAction.setIcon({path: "icons/link-icon.png"});
         }
-
-    }).catch(onError);
-  }
-}
-
-function verifyTabStatus(activeInfo) {
-    browser.tabs.sendMessage(
-        activeInfo.tabId,
-        {isQuery: true}
-    ).then(response => {
-               
-        if (response.hideLinks) {
-             browser.browserAction.setIcon({path: "icons/broken-link-icon.png"});
-        } else {
-            browser.browserAction.setIcon({path: "icons/link-icon.png"});
-        }
-
     }).catch(onError);
 }
 
-// Chaging the icon
-function changeIcon() {
-      browser.browserAction.setIcon({path: "icons/broken-link-icon.png"});
-}
-
-
-function setIcon(tabId, changeInfo, tabInfo) {
-    browser.browserAction.setIcon({path: "icons/link-icon.png"});
-}
-
-const filter = {
-    urls: ["https://*/*", "http://*/*"],
-    properties: ["status"]
-};
 
 // On active tab event to set icon
-//browser.tabs.onUpdated.addListener(setIcon, filter);
 browser.tabs.onActivated.addListener(verifyTabStatus);
 
 
 // On click event
 browser.browserAction.onClicked.addListener(() => {
-  browser.tabs.query({
-    currentWindow: true,
-    active: true
-  }).then(sendMessageToTabs).catch(onError);
+    browser.tabs.query({
+        currentWindow: true,
+        active: true
+    }).then(sendMessageToTabs).catch(onError);
 });
